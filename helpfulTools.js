a88f23145a2883a22ee9ed329eb0ba46341ce38e
crellison <cole.ellison@gmail.com>
2016-10-19 12:18:47 -0400

    make it exported

--- helpfulTools.js
+++ helpfulTools.js
@@ -1,6 +1,6 @@
 // helpful tools
-
-// query builder for http requests
-var buildQuery = (obj) => '?'+Object.keys(obj).map(elt => elt+'='+obj[elt]+'&').join('').slice(0,-1);
-
-const emailRegex = /\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$\b/;
+module.exports = {
+	// query builder for http requests
+	buildQuery : (obj) => '?'+Object.keys(obj).map(elt => elt+'='+obj[elt]+'&').join('').slice(0,-1),
+	emailRegex : /\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$\b/
+};
